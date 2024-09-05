cc9450634802370364665fbc179b7c8344835a38
Vyenktesh Patil <vyenkteshpatil02@gmail.com>
2024-09-05 13:14:57 +0530

    assignment 2 java.Lang.Boolean

---
+++ assignment_2_Boolean/C.java
@@ -0,0 +1,15 @@
+package assignment_2_Boolean;
+
+public class C {
+
+	public static void main(String[] args) {
+		
+		String strStatus = "true";
+		float a = 12f;
+		
+		boolean boolStatus = Boolean.parseBoolean(strStatus);
+//		boolean boolStatus1 = Boolean.parseBoolean(a);
+		
+		System.out.println(boolStatus);
+	}
+}
---
+++ assignment_2_Boolean/E.java
@@ -0,0 +1,14 @@
+package assignment_2_Boolean;
+
+public class E {
+
+	public static void main(String[] args) {
+		
+		boolean status = true;
+		
+		Boolean bool = Boolean.valueOf(status);
+		
+		System.out.println(bool);
+		System.out.println(bool.getClass());
+	}
+}
